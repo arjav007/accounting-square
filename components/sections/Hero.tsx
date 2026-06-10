20db123b9dca75a3c8151c1f52a248ec08211cda
arjav007 <arjavpatni15@gmail.com>
2026-06-10 14:51:17 +0530

    globe fix 2

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -329,7 +329,7 @@ export default function Hero() {
             display: 'flex', 
             alignItems: 'center', 
             justifyContent: 'center',
-            transform: 'translateX(-4%)' /* 👇 This shifts the globe to the left */
+            transform: 'translateX(-8%)' /* 👇 This shifts the globe to the left */
           }}>
             <canvas
               ref={globeCanvasRef}
